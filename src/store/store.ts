460dcbb27d524639a1823c729f1f6792313ffb63
aaldana@procomconsulting.com <aldanam.alexis@gmail.com>
2025-03-06 00:00:51 -0600

    Add Redux toolkit configuration

---
+++ src/store/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import taskReducer from './taskSlice'
+import userReducer from './userSlice'
+
+export const store = configureStore({
+  reducer: {
+    task: taskReducer,
+    user: userReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
---
+++ src/store/taskSlice.ts
@@ -0,0 +1,42 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface Task {
+    id: number;
+    title: string;
+    description?: string;
+    status: "PENDING" | "IN_PROGRESS" | "COMPLETED";
+    assignedTo: number;
+    createdBy: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+interface TaskState {
+    tasks: Task[];
+}
+
+const initialState: TaskState = {
+    tasks: [],
+}
+
+export const taskSlice = createSlice({
+    name: "task",
+    initialState,
+    reducers: {
+        setTasks: (state, action: PayloadAction<Task[]>) => {
+            state.tasks = action.payload;
+        },
+        addTask: (state, action: PayloadAction<Task>) => {
+            state.tasks.push(action.payload);
+        },
+        updateTask: (state, action: PayloadAction<Task>) => {
+            const task = state.tasks.find(task => task.id === action.payload.id);
+            if (task) {
+                task.status = action.payload.status;
+            }
+        }
+    }
+})
+
+export const {setTasks, addTask, updateTask} = taskSlice.actions;
+export default taskSlice.reducer;
